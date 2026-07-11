dd79baba956107b5b68c0fd7a3f23446a1452863
Amos Mwaniki <ndunguamos4320@outlook.com>
2026-07-11 14:57:50 +0000

    moving models to the right folders

--- models/msfabric/marts/dim_customers.sql
+++ models/msfabric/marts/dim_customers.sql
@@ -3,7 +3,7 @@ with customers as (
 select
     *
 
-from {{ref('stg_customers')}}
+from {{ref('stg_jafle_shop__customers')}}
 ),
 
 orders as (
@@ -11,7 +11,7 @@ orders as (
     select
         *
 
-    from {{ref('stg_orders')}}
+    from {{ref('stg_jafle_shop__orders')}}
 ),
 
 customer_orders as (
