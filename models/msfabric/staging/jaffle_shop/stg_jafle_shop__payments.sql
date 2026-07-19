9141881bd18ebd3b8ba77590146eb0438545821e
Amos Mwaniki <ndunguamos4320@outlook.com>
2026-07-19 13:38:40 +0000

    normal changes into the models

--- models/msfabric/staging/jaffle_shop/stg_jafle_shop__payments.sql
+++ models/msfabric/staging/jaffle_shop/stg_jafle_shop__payments.sql
@@ -3,6 +3,7 @@ select
  ORDERID as order_id,
  PAYMENTMETHOD as payment_method,
  STATUS as payment_status,
- AMOUNT as payment_amount,
+ -- amount is stored in cents, convert it to dollars
+ AMOUNT / 100 as payment_amount,
  CREATED as payment_created
 from {{source('streamflix_jaffle','payments')}}
